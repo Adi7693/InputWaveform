bddb6f3764d95ba0963a3bddef0c387b6745d52d
Adi7693 <aditya.mhatre@orion-racing.com>
2018-10-13 03:23:08 +0100

    Initial Commit

---
+++ InputWaveForm/FrequencyData/Frequency.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TimeData;
+
+namespace FrequencyData
+{
+    class Frequency
+    {
+
+        private bool NeedToCalculate;
+        private double _excitationFrequency;
+
+
+        public Frequency(double excitationFrequency, Time time)
+        {
+            ExcitationFrequency = excitationFrequency;
+            Time = time;
+            NeedToCalculate = true;
+
+        }
+
+        private Time _time = null;
+
+        public Time Time
+        {
+            get
+            {
+                return _time;
+            }
+
+            set
+            {
+                if (!value.Equals(_time))
+                {
+                    _time = value;
+
+                    NeedToCalculate = true;
+                }
+            }
+        }
+
+        public double ExcitationFrequency
+        {
+            get
+            {
+                return _excitationFrequency;
+            }
+
+            set
+            {
+                if (!value.Equals(_excitationFrequency))
+                {
+                    _excitationFrequency = value;
+                    NeedToCalculate = true;
+                }
+            }
+        }
+
+        public List<double> CosineOscillation { get; private set; }
+
+        public void CosineCalculate()
+        {
+            if (NeedToCalculate)
+            {
+                if (CosineOscillation == null)
+                {
+                    CosineOscillation = new List<double>();
+                }
+
+                CosineOscillation.Clear();
+
+
+                //Access Time Data from TimeData Project
+                foreach (double item in Time.TimeIntervals)
+                {
+                    double frequency = Math.Cos(2 * Math.PI * ExcitationFrequency * item);
+                    double w = Math.Round(frequency, 6);
+
+                    CosineOscillation.Add(w);
+                }
+
+            }
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            //builder.AppendLine("Times: " + Time.ToString() + "Frequency: " + .ToString() + " Time Step: " + TimeStep.ToString());
+
+            //builder.AppendLine("Start Time: {0:F4}  EndTime:{1:F4}  TimeStep: {2:F4}", StartTime.ToString(), EndTime.ToString(), TimeStep.ToString());
+
+            for (int i = 0; i < Time.TimeIntervals.Count; i++)
+            {
+                builder.AppendLine("Time: " + Time.TimeIntervals[i].ToString() + "\tFrequency:" + CosineOscillation[i].ToString());
+            }
+            return builder.ToString();
+        }
+
+
+
+    }
+}
---
+++ InputWaveForm/TimeData/Time.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeData
+{
+    public class Time
+    {
+        public Time(double startTime, double endTime, double step)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+            TimeStep = step;
+
+            NeedsToRecalculate = true;
+        }
+
+
+        
+
+        private bool NeedsToRecalculate;
+
+        private double _startTime;
+        public double StartTime
+        {
+            get
+            {
+                return _startTime;
+            }
+            set
+            {
+                if (!value.Equals(_startTime))
+                {
+                    _startTime = value;
+
+                    NeedsToRecalculate = true;
+                }
+            }
+        }
+
+        private double _timeStep;
+        public double TimeStep
+        {
+            get
+            {
+                return _timeStep;
+            }
+            set
+            {
+                if (!value.Equals(_timeStep))
+                {
+                    _timeStep = value;
+
+                    NeedsToRecalculate = true;
+                }
+            }
+        }
+
+        private double _endTime;
+        public double EndTime
+        {
+            get
+            {
+                return _endTime;
+            }
+            set
+            {
+                if (!value.Equals(_endTime))
+                {
+                    _endTime = value;
+
+                    NeedsToRecalculate = true;
+                }
+            }
+        }
+
+        public List<double> TimeIntervals { get; private set; }
+
+        public void Calculate()
+        {
+            if (NeedsToRecalculate)
+            {
+                if (TimeIntervals == null)
+                {
+                    TimeIntervals = new List<double>();
+                }
+
+                TimeIntervals.Clear();
+
+                for (double i = StartTime; i <= EndTime + TimeStep / 2.0; i += TimeStep)
+                {
+                    double interval = Math.Round(i, 6);
+                    TimeIntervals.Add(interval);
+                }
+
+                NeedsToRecalculate = false;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine("Start Time: " + StartTime.ToString() + " End Time: " + EndTime.ToString() + " Time Step: " + TimeStep.ToString());
+
+            //builder.AppendLine("Start Time: {0:F4}  EndTime:{1:F4}  TimeStep: {2:F4}", StartTime.ToString(), EndTime.ToString(), TimeStep.ToString());
+
+            for (int i = 0; i < TimeIntervals.Count; i++)
+            {
+                builder.AppendLine("Interval " + i.ToString() + ": " + TimeIntervals[i].ToString());
+            }
+            return builder.ToString();
+        }
+
+    }
+}
